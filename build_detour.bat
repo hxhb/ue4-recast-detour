3e83b244b990a0b7cd3175efcb21870844d04507
hxhb <hi@imzlp.com>
2021-05-24 01:35:53 +0000

    add compile script

--- build_detour.bat
+++ build_detour.bat
@@ -1,2 +1,2 @@
-g++ -std=c++11 -o ue4-detour.exe ue4-detour.cpp UE4RecastHelper.h UE4RecastHelper.cpp Detour/*.cpp Detour/*.h
+g++ -std=c++11 -o ue4-detour.exe ue4-detour.cpp UE4RecastHelper.h UE4RecastHelper.cpp Detour/*.cpp Detour/*.h -static
 pause
--- build_findpath.bat
+++ build_findpath.bat
@@ -1,1 +1,1 @@
-g++ -std=c++11 -o ue4-findpath.exe ue4-findpath.cpp UE4RecastHelper.h UE4RecastHelper.cpp Detour/*.cpp Detour/*.h
+g++ -std=c++11 -o ue4-findpath.exe ue4-findpath.cpp UE4RecastHelper.h UE4RecastHelper.cpp Detour/*.cpp Detour/*.h -static
